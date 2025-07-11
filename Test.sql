5dd1a69af96c24cd2af857fb2f167fb5b448d5e8
krajeevs <77295781+krajeevs@users.noreply.github.com>
2025-07-11 18:49:28 +0530

    Update Test.sql

--- Test.sql
+++ Test.sql
@@ -14,3 +14,24 @@ WHERE
 ORDER BY
     table_name,
     trigger_name;
+
+--Version 2
+
+SELECT
+    trig.name AS trigger_name,
+    tab.name AS table_name,
+    trig.crdate AS creation_date,
+    CASE
+        WHEN (trig.sysstat & 2048) = 2048 THEN 'Disabled'
+        ELSE 'Enabled'
+    END AS trigger_state
+FROM
+    sysobjects trig
+    JOIN sysdepends dep ON trig.id = dep.id
+    JOIN sysobjects tab ON dep.depid = tab.id
+WHERE
+    trig.type = 'TR'
+ORDER BY
+    tab.name,
+    trig.name;
+
